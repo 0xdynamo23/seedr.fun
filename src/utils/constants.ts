28826ee5ff63da1e3bbb49f9c4a226e780079365
GitBolt <saabisakhtar@gmail.com>
2025-03-20 21:16:05 +0530

    switch to mainnet

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,13 +3,15 @@ export const CONTRACTS = {
   }
   
   export const TREASURY = {
-    treasury: 'xion1txs88wlks2sk4unelt7xasaarzvmvcmu3lsyuafzacd2zhr3ymzsry4x4j'
+    treasury: 'xion1txs88wlks2sk4unelt7xasaarzvmvcmu3lsyuafzacd2zhr3ymzsry4x4j',
+    rpcUrl: "https://rpc.xion-mainnet-1.burnt.com:443",
+    restUrl: "https://api.xion-mainnet-1.burnt.com:443",
   }
   
   export const CONTRACTS_ARRAY = Object.values(CONTRACTS)
   
   
-  export const CHAIN_ID = 'xion-testnet-1'
+  export const CHAIN_ID = 'xion-mainnet-1'
   
   export const EXPLORER = `https://explorer.burnt.com/${CHAIN_ID}`
   
